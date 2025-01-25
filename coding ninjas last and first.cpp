fd1582d87521f7777fd5c515b60f3ecccb5daa37
shruti-605 <114794288+shruti-605@users.noreply.github.com>
2025-01-25 16:13:59 +0530

    Create coding ninjas last and first.cpp

---
+++ coding ninjas last and first.cpp
@@ -0,0 +1,28 @@
+int lastOcc(vector<int>& arr,int n,int key){
+	int s=0;
+	int e=n-1;
+	int mid=s+(e-s)/2;
+	int ans=-1;
+	while(s<=e){
+		if(arr[mid]==key){
+			ans=mid;
+			s=mid+1;
+		}
+		else if(key>arr[mid]){
+			s=mid+1;
+		}else if(key<arr[mid]){
+			e=mid-1;
+		}
+		mid=s+(e-s)/2;
+	}
+	return ans;
+}
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
+{
+    pair<int,int>p;
+    p.first=firstOcc(arr,n, k);
+    p.second=lastOcc(arr,n,k);
+
+    return p;
+}
