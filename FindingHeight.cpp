d50675a3380b0460eed540f06f949e753d3117a4
shruti-605 <114794288+shruti-605@users.noreply.github.com>
2025-02-05 15:56:27 +0530

    Create FindingHeight.cpp

---
+++ FindingHeight.cpp
@@ -0,0 +1,11 @@
+  int height(struct Node* node){
+           //base case
+           if(node==NULL){
+               return 0;
+           }
+           int left=height(node->left);
+           int right=height(node->right);
+           
+           int ans=max(left,right)+1;
+           return ans;
+       }
