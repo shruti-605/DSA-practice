fcfd5c9aa4e49ca95dbeae9e559e4007d4acc366
shruti-605 <114794288+shruti-605@users.noreply.github.com>
2025-02-17 15:03:35 +0530

    Update BSTBasic.cpp

--- BSTBasic.cpp
+++ BSTBasic.cpp
@@ -95,6 +95,60 @@ void takeInput(Node* &root){
 	}
 	
 }
+Node* minVal(Node* root){
+	Node* temp=root;
+	while(temp->left!=NULL){
+		temp=temp->left;
+	}
+	return temp;
+}
+Node* maxVal(Node* root){
+	Node* temp=root;
+	while(temp->right!=NULL){
+		temp=temp->right;
+	}
+	return temp;
+}
+Node* deleteFromBST(Node* root,int val){
+	//base case
+	if(root==NULL){
+		return root;
+	}
+	if(root->data==val{
+		//0 child
+		if(root->left==NULL && root->right==NULL){
+			delete root;
+			return NULL;
+		}
+		//1 child
+		//left child
+		if(root->left!=NULL && root->right==NULL){
+			Node* temp=root->left;
+			delete root;
+			return temp;
+		}
+		//right child
+		if(root->left==NULL && root->right!=NULL){
+			Node* temp= root->right;
+			delete root;
+			return temp;
+		}
+		//2 child
+		if(root->left!=NULL && root->right!=NULL){
+			int mini=minVal(root->right)->data;
+			root->data=mini;
+			root->right=deleteFromBST(root->right,mini)
+			return root;
+		}
+	}
+	else if(root->data>val){
+		//left part me jao
+		root->left=deleteFromBST(root->left,val);
+	}else{
+		//right part me jao
+		root->right=deleteFromBST(root->right,val);
+		return root;
+	}
 int main(){
 	Node* root=NULL;
 	
